4e179406c3ca415dbe448e3717fa248647371957
Anders Näsman <anasman@akamai.com>
2023-10-09 20:43:13 +0200

    Added Elastic store function

--- src/akamai.js
+++ src/akamai.js
@@ -5,12 +5,26 @@ const loglevel = 'debug';
 const express = require('express');
 const app = express();
 const cors = require('cors');
+const crypto = require('crypto');
 const EdgeAuth = require('akamai-edgeauth');
 var EdgeGrid = require('akamai-edgegrid');
 const config = require("config");
+const { Client } = require('@elastic/elasticsearch');
+var EdgeGrid = require('akamai-edgegrid');
    
 var eg = new EdgeGrid(config.get("akamaiAuth.clientToken"), config.get("akamaiAuth.clientSecret"), config.get("akamaiAuth.accessToken"), config.get("akamaiAuth.baseUri"));
  
+// Define the encryption details
+const algorithm = 'aes-256-cbc';
+const secret = '1111111111your-shared-secret-key';  // This must be 256 bits (32 characters)
+const ivLength = 16;  // For AES, this is always 16
+
+
+const esclient = new Client({
+  node: config.get("elasticdb.url")
+});
+
+
 //Get date time
 function timestamp() {
 	let date_ob = new Date();
@@ -39,6 +53,91 @@ function log(msg,msgtype){
 		if (msgtype == 'error'){console.error(msg);}}
 	}
 
+function encrypt(text) {
+	let iv = crypto.randomBytes(ivLength);
+	let cipher = crypto.createCipheriv(algorithm, Buffer.from(secret), iv);
+	let encrypted = cipher.update(text);
+	
+	encrypted = Buffer.concat([encrypted, cipher.final()]);
+	
+	// iv needed for decryption, so we will concatenate with encrypted message
+	return iv.toString('hex') + ':' + encrypted.toString('hex');
+	}
+	
+function decrypt(text) {
+	let textParts = text.split(':');
+	let iv = Buffer.from(textParts.shift(), 'hex');
+	let encryptedText = Buffer.from(textParts.join(':'), 'hex');
+	let decipher = crypto.createDecipheriv(algorithm, Buffer.from(secret), iv);
+	let decrypted = decipher.update(encryptedText);
+	
+	decrypted = Buffer.concat([decrypted, decipher.final()]);
+	
+	return decrypted.toString();
+	}
+	  
+	function getUniqueSidsForHmac(hmac) {
+		return client.search({
+		  index: 'antipiracy', // replace with your index name
+		  body: {
+			size: 0,
+			query: {
+			  term: {
+				hmac: hmac // this will filter documents that have the specific hmac
+			  }
+			},
+			aggs: {
+			  unique_sids: {
+				terms: {
+				  field: 'sid.keyword', // this will group documents by sid
+				  size: 1000 // adjust this number based on your needs
+				}
+			  }
+			}
+		  }
+		})
+		.then(response => {
+			console.log('Elasticsearch response:', JSON.stringify(response.body));
+			// extract and return the unique sids from the response
+			return response.body.aggregations.unique_sids.buckets.map(bucket => bucket.key);
+		  })
+		.catch(error => {
+		  console.error('Elasticsearch query failed:', error);
+		});
+	  }
+
+	async function storeSession(sid,fraud) {
+		console.log("Saving: "+sid);
+		try {
+			
+			const response = await esclient.reindex({
+				body: {
+					source: {
+						index: 'antipiracy',
+						query: {
+							term: {
+								'sid.keyword': sid.toString()
+							}
+						}
+					},
+					dest: {
+						index: 'storedsessions'
+					},
+					script: {
+						source: 'ctx._source.fraud = params.fraud',  // Use params.fraud here
+						params: {
+							fraud: fraud  // Assign your dynamic value to the fraud parameter
+						}
+					}
+				}
+			});
+		
+			console.log('Reindex operation response:', response.body);
+		} catch (error) {
+			console.log('Error during reindexing:'+error,'error');
+		}
+	}
+
 //Catch all requests and log them
 app.use(express.json());
 
@@ -57,12 +156,15 @@ app.get('/test', (req, res) => {
 
 app.get('/createToken', (req, res) => {
 	log('createToken');
+	if (req.query.auth == "_Akama1zedTV2T0ken_")
+	{
 		var ea = new EdgeAuth({key: config.get("token.key"), windowSeconds: 5000, escapeEarly: true, verbose: false});
 		var token = ea.generateACLToken("/*");
 		const data = {
 			token: token
 			};
 		res.status(200).json(data);	
+	} else res.status(404).send('{"status":"access denied"}');
 });
 
 app.get('/blockToken', (req, res) => {
@@ -92,6 +194,25 @@ app.get('/blockToken', (req, res) => {
 		});		
 	} else res.status(404).send('Missing or flaw id');
 });
+
+app.get('/storeSession', (req, res) => {
+    log('storeSession ' + req.query.id);
+
+    const isFraud = req.query.fraud === 'true';
+
+    if (req.query.id && req.query.id !== "" && 'fraud' in req.query && req.query.fraud !== "") {
+        storeSession(req.query.id, isFraud);
+        res.status(200).json({ status: "ok" });
+    } else {
+        if (!req.query.id || req.query.id === "") {
+            res.status(404).json({ status: "error missing id" });
+        } else if (!('fraud' in req.query) || req.query.fraud === "") {
+            res.status(400).json({ status: "error missing or empty fraud parameter" });
+        }
+    }
+});
+
+
 
 app.get('/unblockToken', (req, res) => {
 	log('unblockToken '+req.query.id);
@@ -134,10 +255,30 @@ app.get('/listTokens', (req, res) => {
 	});	
 });
 
-//The following methods responds to a Grafana JSON request
+
+app.get('/blockList/listSessionIDs', (req, res) => {
+
+	const hmac = req.query.hmac;
+	log('listSesstionIDs (hmac: '+hmac+')');
+
+	getUniqueSidsForHmac(hmac) 
+	.then(uniqueSids => console.log('Unique SIDs:', uniqueSids))
+	.catch(error => console.error('Failed to get unique SIDs:', error));
+
+/*
+	let message = 'Hello, World!';
+	let encryptedMessage = encrypt(message);
+
+	console.log('Original Message: ' + message);
+	console.log('Encrypted Message: ' + encryptedMessage);
+	console.log('Decrypted Message: ' + decrypt(encryptedMessage));
+	
+	res.status(200).send("ok");*/
+});
+
+
 app.get('/blockList/listTokens', (req, res) => {
 	log('Query');
-	//let data = {"listItems":[{"id":"Fabolus","ttl":17993}]};
 	eg.auth({
 		path: config.get("akamaiEdgeRC.basePath"), 
 		method: 'GET',
@@ -145,6 +286,7 @@ app.get('/blockList/listTokens', (req, res) => {
 			'Accept': "application/json"
 		}
 	});
+
 	eg.send(function(error, response, body) {
 		log('Error: '+error);
 		log('Body: '+body);
